d8d2c367d5b82556ef61867a30536e9fc5aa36d4
Joseph Valdez <Joseph.W.Valdez@GMail.com>
2024-01-13 21:04:39 -0800

    Created basic function for updating message max widths

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,13 +1,20 @@
-// contentScript.ts
-
-// This script runs in the context of web pages
-
 console.log("Content script loaded.");
 
-// Example: Changing the background color of the page
+// changing the background color of the page
 document.body.style.backgroundColor = '#343541';
 
-// Example: Listening for messages from the background script or popup
+
+const customStyle = document.createElement('style');
+document.head.appendChild(customStyle);
+
+const updateMaxWidth = (widthPercentage: number) => {
+    customStyle.textContent = `[data-testid] > * > * { max-width: ${widthPercentage}%} }`
+}
+
+// default setting for 100% max width
+updateMaxWidth(95)
+
+// listening for messages from the background script or popup
 chrome.runtime.onMessage.addListener(
   (request, sender, sendResponse) => {
     if (request.message === 'change_color') {
@@ -16,7 +23,7 @@ chrome.runtime.onMessage.addListener(
     }
 });
 
-// Send a message to the background script if needed
+// send a message to the background script if needed
 chrome.runtime.sendMessage({ message: 'Content script active' }, response => {
   console.log(response.reply);
 });
