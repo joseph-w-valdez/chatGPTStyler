e1a8e9e46438fb4d636d35f62afe80cc8eaefc2d
Kevin Lay <kevinlay@live.com>
2024-01-23 11:36:28 -0800

    17 - Improve Color Scheme for Enhanced Visibility
    -Added function named getComplementaryColor that gets the complementary color of another color.
    -Added function to change selection colors of selected text.

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,42 +1,59 @@
 console.log("Content script loaded.");
 
-const customStyle = document.createElement('style');
+const customStyle = document.createElement("style");
 document.head.appendChild(customStyle);
 
-let messageMaxWidthStyle = '';
-let messagePaddingStyle = ''; 
-let messageBorderRadiusStyle = '';
-let inputBoxMaxWidthStyle = '';
-let messageBoxColors = '';
-let selectionColors = '';
+let messageMaxWidthStyle = "";
+let messagePaddingStyle = "";
+let messageBorderRadiusStyle = "";
+let inputBoxMaxWidthStyle = "";
+let messageBoxColors = "";
+let selectionColors = "";
 let chatMessageButtons = `
     [data-testid] button {
       visibility: unset
     }
 `;
 
-const updateMessageColor = (color: string, isUser: boolean, isDark: boolean) => {
+const updateMessageColor = (
+    color: string,
+    isUser: boolean,
+    isDark: boolean,
+) => {
     messageBoxColors = `
       .dark {
-        [data-testid]:nth-child(even) > * > * { background-color: ${isUser && isDark ? color : '#4e7645'} }
-        [data-testid]:nth-child(odd) > * > * { background-color: ${!isUser && isDark ? color : '#3c6083'} }
+        [data-testid]:nth-child(even) > * > * { background-color: ${
+            isUser && isDark ? color : "#4e7645"
+        } }
+        [data-testid]:nth-child(odd) > * > * { background-color: ${
+            !isUser && isDark ? color : "#3c6083"
+        } }
       }
       .light {
-        [data-testid]:nth-child(even) > * > * { background-color: ${isUser && !isDark ? color : '#62B1F6'} }
-        [data-testid]:nth-child(odd) > * > * { background-color: ${!isUser && !isDark ? color : '#EEEEEE'} }
+        [data-testid]:nth-child(even) > * > * { background-color: ${
+            isUser && !isDark ? color : "#62B1F6"
+        } }
+        [data-testid]:nth-child(odd) > * > * { background-color: ${
+            !isUser && !isDark ? color : "#EEEEEE"
+        } }
       }
     `;
-    updateAllStyles();
 };
-
-const updateAllStyles = () => {
-    customStyle.textContent = messageBoxColors + messageMaxWidthStyle + messagePaddingStyle 
-                              + messageBorderRadiusStyle + inputBoxMaxWidthStyle + selectionColors + chatMessageButtons;
-}
 
 const updateMessageMaxWidth = (widthPercentage: number) => {
     messageMaxWidthStyle = `@media (min-width: 1200px) { [data-testid] > * > * { max-width: ${widthPercentage}% } }`;
     updateAllStyles();
+};
+
+const updateAllStyles = () => {
+    customStyle.textContent =
+        messageBoxColors +
+        messageMaxWidthStyle +
+        messagePaddingStyle +
+        messageBorderRadiusStyle +
+        inputBoxMaxWidthStyle +
+        selectionColors +
+        chatMessageButtons;
 };
 
 const updateMessagePadding = (padding: string) => {
@@ -54,10 +71,10 @@ const updateInputBoxMaxWidth = (widthPercentage: number) => {
       form { max-width: ${widthPercentage}% !important; }
     }`;
     updateAllStyles();
-}
+};
 
 const resetDefaultMessageColors = () => {
-  messageBoxColors = `
+    messageBoxColors = `
   .dark {
     [data-testid]:nth-child(even) > * > * { background-color: #4e7645 }
     [data-testid]:nth-child(odd) > * > * { background-color: #3c6083 }
@@ -67,30 +84,51 @@ const resetDefaultMessageColors = () => {
     [data-testid]:nth-child(odd) > * > * { background-color: #EEEEEE }
   }
 `;
-  updateAllStyles();
-}
+};
+
+// updates the text color and background color of highlighted/selected text. first selector targets user messages. second selector targets received messages
+const updateSelectionColors = (textColor: string, isUser: boolean) => {
+    selectionColors = `
+    [data-message-id] > *::selection { background-color: ${
+        isUser ? getComplementaryColor(textColor) : "black"
+    }; color: ${isUser ? textColor : "white"} }
+    [data-message-id] > * *::selection { background-color: ${
+        !isUser ? getComplementaryColor(textColor) : "black"
+    }; color: ${!isUser ? textColor : "white"} }
+  `;
+    updateAllStyles();
+};
 
 const setDefaultSettings = () => {
     updateMessageMaxWidth(95);
-    updateMessagePadding('10px');
-    updateMessageBorderRadius('5px');
+    updateMessagePadding("10px");
+    updateMessageBorderRadius("5px");
     updateInputBoxMaxWidth(70);
     resetDefaultMessageColors();
-}
+};
+
+const getComplementaryColor = (color = "") => {
+    const colorPart = color.slice(1);
+    const ind = parseInt(colorPart, 16);
+    let iter = ((1 << (4 * colorPart.length)) - 1 - ind).toString(16);
+    while (iter.length < colorPart.length) {
+        iter = "0" + iter;
+    }
+    return "#" + iter;
+};
 
 // set default settings on load
 setDefaultSettings();
 
 // listening for messages from the background script or popup
-chrome.runtime.onMessage.addListener(
-  (request, sender, sendResponse) => {
-    if (request.message === 'change_color') {
-      document.body.style.backgroundColor = request.color;
-      sendResponse({status: 'Color changed'});
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.message === "change_color") {
+        document.body.style.backgroundColor = request.color;
+        sendResponse({ status: "Color changed" });
     }
 });
 
 // send a message to the background script if needed
-chrome.runtime.sendMessage({ message: 'Content script active' }, response => {
-  console.log(response.reply);
+chrome.runtime.sendMessage({ message: "Content script active" }, (response) => {
+    console.log(response.reply);
 });
